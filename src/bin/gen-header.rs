b7f49ec27c06101dc780ea9bf348d3f09a05a286
pan93412 <pan93412@gmail.com>
2022-08-06 19:51:19 +0800

    refactor(header): allow file customization

--- src/bin/gen-header.rs
+++ src/bin/gen-header.rs
@@ -1,4 +1,4 @@
 #[cfg(feature = "headers")]
 fn main() -> anyhow::Result<()> {
-    wmjtyd_libstock_ffi::header::generate_header()
+    wmjtyd_libstock_ffi::header::generate_header(::std::env::args_os().nth(1).as_deref())
 }
--- src/header.rs
+++ src/header.rs
@@ -1,13 +1,24 @@
 //! For `bin/gen-header.rs`. Generate the C header.
 
-pub fn generate_header() -> anyhow::Result<()> {
+use std::ffi::OsStr;
+
+pub fn generate_header(filename: Option<&OsStr>) -> anyhow::Result<()> {
     use anyhow::Context;
 
-    ::safer_ffi::headers::builder()
-        .to_file("libstock.h")
-        .context("Failed to create builder.")?
-        .generate()
-        .context("Failed to generate header.")?;
+    let builder = ::safer_ffi::headers::builder();
+
+    if let Some(filename) = filename {
+        builder
+            .to_file(filename)
+            .context("Failed to create builder.")?
+            .generate()
+            .context("Failed to generate header.")?;
+    } else {
+        builder
+            .to_writer(::std::io::stdout())
+            .generate()
+            .context("Failed to generate header.")?;
+    }
 
     Ok(())
 }
